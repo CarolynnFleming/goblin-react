b69ae4472c44085132c203a9ac690f15daee923a
CarolynnFleming <cfcatpink@gmail.com>
2022-02-02 20:53:49 -0500

    filter working

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
     // use the filter method to get an array of goblins whose name includes this search argument
     const lesGoblins = allGoblins.filter(goblin => goblin.name.includes(search));
     // if there is a search argument, set the filtered goblins to the filtered goblins
-    search ? setFilteredGoblins(lesGoblins) : filteredGoblins(allGoblins);
+    search ? setFilteredGoblins(lesGoblins) : setFilteredGoblins(allGoblins);
     // if the search argument is undefined, set the filtered goblins in state to just be the array of all goblins
   }
 
@@ -88,7 +88,7 @@ function App() {
         
       />
       <GoblinList 
-        goblins={allGoblins} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array 
+        goblins={filteredGoblins.length ? filteredGoblins : allGoblins} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array 
         handleDeleteGoblin={handleDeleteGoblin} // note that the goblin list has access to the ability to delete
       />
     </div>
