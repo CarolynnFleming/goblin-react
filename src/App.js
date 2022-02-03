dd29065410b4fd890429744d166aec60649ac669
CarolynnFleming <cfcatpink@gmail.com>
2022-02-02 20:29:40 -0500

    filter method used to get an array

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
 
   function handleFilterGoblins(search) {
     // use the filter method to get an array of goblins whose name includes this search argument
-
+    const lesGoblins = allGoblins.filter(goblin => goblin.name.includes(search));
     // if there is a search argument, set the filtered goblins to the filtered goblins
     // if the search argument is undefined, set the filtered goblins in state to just be the array of all goblins
   }
